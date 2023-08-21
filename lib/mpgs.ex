95b73b33a015b4d62aab9e6a9f0a84a3f8d03204
Abdullah Esmail <aesmail@hey.com>
2023-08-21 09:24:02 +0300

    improve refund_transaction docs.

--- lib/mpgs.ex
+++ lib/mpgs.ex
@@ -302,6 +302,23 @@ defmodule Mpgs do
   - `api_base`. Optional. Defaults to #{@api_url}.
   - `api_version`. Optional. Defaults to #{@api_version}.
   - `api_merchant`. Required.
+
+  Example:
+  ```
+  order_params = %{
+    api_username: "my-mpgs-username",
+    api_password: "my-mpgs-password",
+    api_merchant: "my-mpgs-merchant",
+    order: "0123456789",
+    trx: "0987654321",
+    amount: "15",
+    currency: "KWD"
+  }
+
+  {:ok, response} = Mpgs.refund_transaction(params)
+  response["result"] #=> "SUCCESS"
+  response["order"]["status"] #=> "REFUNDED"
+  ```
   """
   def refund_transaction(params) do
     order = get_local_var(params, :order)
